349db6b6c4b2cd407b3095fcaed24c437f3a4e25
Behrooz <pahlavan111@gmail.com>
2023-01-09 16:12:18 +0330

    create AppModule object

---
+++ app/src/main/java/com/bp/main/hilt/AppModule.kt
@@ -0,0 +1,26 @@
+package com.bp.main.hilt
+
+import com.bp.main.api.WebService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+
+    @Singleton
+    @Provides
+    fun provideApiCall(): WebService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WebService::class.java)
+    }
+}
