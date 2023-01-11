2483d52bb84af90b39a42457993118996e2a8f0a
Behrooz <pahlavan111@gmail.com>
2023-01-12 00:52:17 +0330

    Add Photo data class

---
+++ app/src/main/java/com/bp/main/model/Photo.kt
@@ -0,0 +1,9 @@
+package com.bp.main.model
+
+data class Photo(
+    val albumId: Int? = null,
+    val id: Int? = null,
+    val title: String? = null,
+    val url: String? = null,
+    val thumbnailUrl: String? = null
+)
