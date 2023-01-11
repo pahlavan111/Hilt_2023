200208e5f122315a3ef0c60623cce29d77b84e83
Behrooz <pahlavan111@gmail.com>
2023-01-12 00:55:13 +0330

    comment RemoteErrorEmitter from ApiCall

--- app/src/main/java/com/bp/main/api/ApiCall.kt
+++ app/src/main/java/com/bp/main/api/ApiCall.kt
@@ -3,6 +3,7 @@ package com.bp.main.api
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import dagger.Provides
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.withContext
@@ -11,7 +12,6 @@ import retrofit2.HttpException
 import java.io.IOException
 
 private suspend fun <T> privateApiCall(
-    emitter: RemoteErrorEmitter,
     responseFunction: suspend () -> T
 ): T? {
     try {
@@ -20,34 +20,34 @@ private suspend fun <T> privateApiCall(
         }
 
     } catch (e: Exception) {
-        withContext(Dispatchers.Main) {
-            e.printStackTrace()
-            Log.e("ApiCal", "callError: ${e.localizedMessage}", e.cause)
-            when (e) {
-                is HttpException -> {
-                    val body = e.response()?.errorBody()
-                    emitter.onError(body.toString())
-                }
-                is TimeoutCancellationException -> {
-                    emitter.onError(ErrorType.TIMEOUT)
-                }
-                is IOException -> {
-                    emitter.onError(ErrorType.NETWORK)
-                }
-                else -> emitter.onError(ErrorType.UNKNOWN)
-            }
-        }
+//        withContext(Dispatchers.Main) {
+//            e.printStackTrace()
+//            Log.e("ApiCal", "callError: ${e.localizedMessage}", e.cause)
+//            when (e) {
+//                is HttpException -> {
+//                    val body = e.response()?.errorBody()
+//                    emitter.onError(body.toString())
+//                }
+//                is TimeoutCancellationException -> {
+//                    emitter.onError(ErrorType.TIMEOUT)
+//                }
+//                is IOException -> {
+//                    emitter.onError(ErrorType.NETWORK)
+//                }
+//                else -> emitter.onError(ErrorType.UNKNOWN)
+//            }
+//        }
         return null
     }
 }
 
  fun <T> apiCall(
-    emitter: RemoteErrorEmitter,
+//    emitter: RemoteErrorEmitter,
     responseFunction: suspend () -> T
 ): LiveData<T?> {
 
     return liveData {
-        val response = privateApiCall(emitter) {
+        val response = privateApiCall() {
             responseFunction()
         }
         emit(response)
